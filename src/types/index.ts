4c1546ce49bec439e3f869734daedc30d3aa139a
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-03-06 16:58:26 +0300

    feat: add description of view components / add events

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export interface ICatalog {
     data: IProduct[];
-    selectedItem: IProduct;
+    selectedItem: IProduct | null;
 }
 
 export interface IProduct {
@@ -29,4 +29,19 @@ export interface IOrder {
 export interface IModal<T> {
     state: boolean;
     content: T;
+}
+
+enum Events {
+    CATALOG_UPDATE = 'catalog:changed',
+    PRODUCT_SELECT = 'catalog:selected',
+    PRODUCT_UPDATE = 'product:changed',
+    CART_UPDATE = 'cart:changed',
+    ORDER_UPDATE = 'order:changed',
+
+    UI_PRODUCT_SELECT = 'ui:catalog-selected',
+    UI_PRODUCT_SHOW = 'ui:product-show',
+    UI_CART_SHOW = 'ui:cart-show',
+    UI_FORM_SUBMIT = 'ui:form-submit',
+    UI_MODAL_SHOW = 'ui:modal-show',
+    UI_MODAL_HIDE = 'ui:modal-hide'
 }
