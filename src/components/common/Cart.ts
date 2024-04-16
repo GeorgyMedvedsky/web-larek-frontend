e6e722ac0dcddeb8755a916f63922e6ce4355135
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-04-16 14:38:25 +0300

    feat: add basket__item-index

--- src/components/common/Cart.ts
+++ src/components/common/Cart.ts
@@ -6,6 +6,7 @@ import {cloneTemplate, createElement, ensureElement} from "../../utils/utils";
 export class Cart extends View<ICartView> {
     protected _list: HTMLElement;
     protected _total: HTMLElement;
+    protected _itemIndex: HTMLElement;
     // protected _button: HTMLElement;
 
     constructor(container: HTMLElement, protected events: EventEmitter) {
@@ -13,6 +14,7 @@ export class Cart extends View<ICartView> {
 
         this._list = ensureElement<HTMLElement>('.basket__list', this.container);
         this._total = this.container.querySelector('.basket__price');
+        
         // this._button = this.container.querySelector('.basket__action');
 
         // if (this._button) {
@@ -27,6 +29,10 @@ export class Cart extends View<ICartView> {
     set items(items: HTMLElement[]) {
         if (items.length) {
             this._list.replaceChildren(...items);
+            items.forEach((item, index) => {
+                this._itemIndex = item.querySelector('.basket__item-index');
+                this.setText(this._itemIndex, index + 1);
+            });
         } else {
             this._list.replaceChildren(createElement<HTMLParagraphElement>('p', {
                 textContent: 'Корзина пуста'
